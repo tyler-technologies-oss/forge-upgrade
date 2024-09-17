c70fcb2d655c7c05d6ad25021ea485e1ef353516
Sam Richardson <sam.richardson@tylertech.com>
2024-09-17 15:23:51 -0400

    feat: add migration from has-padding to no-padding

--- migrations/html/v3/posthtml-forge-card.mjs
+++ migrations/html/v3/posthtml-forge-card.mjs
@@ -1,10 +1,17 @@
 import matchHelper from 'posthtml-match-helper';
-import { removeAttr } from '../posthtml-helpers.mjs';
+import { removeAttr, setAttr } from '../posthtml-helpers.mjs';
 
 export default function transform(tree) {
   // Remove the `outlined` attribute from forge-card
   tree.match(matchHelper('forge-card[outlined]'), node => {
     removeAttr(node, 'outlined');
+    return node;
+  });
+
+  // Change the `has-padding="false"` attribute to `no-padding`
+  tree.match(matchHelper('forge-card[has-padding="false"]'), node => {
+    removeAttr(node, 'has-padding');
+    setAttr(node, 'no-padding', '');
     return node;
   });
 }
